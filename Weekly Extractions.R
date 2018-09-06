177ebba60e2fcb05904f867d9cba51667cec63e3
brunocarlin <brunotestaguzza@gmail.com>
2018-09-06 17:48:15 -0300

    Did gods messy bad work

--- Weekly Extractions.R
+++ Weekly Extractions.R
@@ -7,31 +7,56 @@ library(hts)
 library(tidyverse)
 library(furrr)
 library(foreach)
-#Daily_Collections_AR$Column1 <- as.Date(Daily_Collections_AR$Column1, "%d/%m/%Y")
+library(xts)
+library(thief)
+Daily_Collections_AR$Chave <- as.Date(Daily_Collections_AR$Chave, "%Y/%m/%d")
 
 Tibble_Frame <- as.tsibble(Daily_Collections_AR, index = Chave)
 
 ped_gaps <- Tibble_Frame %>%
-  fill_na(.full = 0)
+  fill_na(.)
 
 Weeks <- Tibble_Frame %>%
-  index_by(year_week = yearweek(Chave)) %>%
+  group_by(year_week = yearweek(Chave)) %>%
   mutate_all(funs(as.numeric)) %>% 
   summarise_all(funs(sum),na.rm = T)
 
+Weeks <- Tibble_Frame[,-1] %>%
+  summarise_all(tile_dbl(., mean, .size = 7,na.rm = TRUE))
 
 
 
+Weekly <- map_df(ped_gaps[,-1],function(col) tile_dbl(col,mean, .size = 7,na.rm = TRUE))
+
+Teste_Sum <- Weekly[,1]
+T
+Teste_Sum2 <- ped_gaps[,2]%>% sum(na.rm = T)
+head(Teste_Sum2,14)
+Some <- Teste_Sum2[8:14,] %>% as.data.frame() %>% as.vector()
+Some2 <- as.numeric(unlist(Some))
+sum(Some2, na.rm = T)
+tail(Teste_Sum2,7) %>% sum(na.rm = T)
 # Example 2 with two simple hierarchies (geography and product) to show the argument "characters"
 
+Test <- ped_gaps[,-1]
 
+xts(x=Tibble_Frame[,-1], order.by=Tibble_Frame[,1])
 
-x1 <- gts(Weeks[,-1], characters = list(c(5, 5), c(5, 5)))
+Y_Season<- msts(Test, seasonal.periods=c(7,365.25), start = c(2011))
+
+x1 <- gts(Y_Season, characters = list(c(5, 5), c(5, 5)))
+frequency(x1)
+
 
 
 h <- 12
 ally <- aggts(x1)
-ally1 <- msts(ally, seasonal.periods=c(4.35,52.18))
+frequency(x1) <- 2
+
+
+Testar <- as.tsibble(ally1)
+
+
 str(ally1)
 str(ally)
 allf <- matrix(NA,nrow = h,ncol = ncol(ally1))
@@ -40,16 +65,34 @@ foreach(i = 1:ncol(ally1)) %dopar% {
 }
 
 Mean_Tbats <- function(y,h = 12) {
-  forecast(tbats(y),h)$mean
+  forecast(tbats(y),h,lambda = 0)$mean
 }
+
+Mean_stlm <- function(y,h = 12) {
+  forecast(thief(y),h)$mean
+}
+forecast(stlm(ally2),12,lambda ="auto")
+ally2 <- ally[,1]
 forecast(tbats(ally2),12)$mean
-Mean_Tbats(ally2)
+
+sum(Mean_Tbats(ally2,31))
+sum(Mean_stlm(ally2,31))
+
+T <- tbats(ally2)
+
 map(ally2,Mean_Tbats)
-ally2 <- ally1[,1]
+
 teste <- lapply(ally1,Mean_Tbats)
 allf <- as.ts(allf, start = 51)
 y.f <- combinef(allf, groups = get_groups(x1), keep ="bottom", algorithms = "lu")
 
+Teste2 <- forecast(
+  ally, h = 10, method = "comb", algorithms = "lu",
+  FUN = function(x) tbats(x, use.parallel = FALSE)
+)
 
+tail(ally2)
+
+y.f[,1]
 plot(y.f)
 ## End(Not run)
